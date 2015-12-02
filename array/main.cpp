29ccf47916b63abbe34fda5eec69df851af53edb
1004385346 <1004385346@qq.com>
2015-12-02 18:31:15 +0800

    数组越界问题
    简单来说就是当一个数组使用的空间超出定义时系统给他分配的空间，就会占用其它数组或变量的使用空间。就这个程序来探讨这个问题，我先定义一个长度为3的
    a数组并赋值，输出a数组的所有值。然后定义一个长度为4的b数组。输出a/b数组的地址。在给b数组赋10个数，超出定义长度4，6个单位，再输出a数
    组。发现a数组在不做改变的情况下值改变了。

---
+++ array/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  array
+//
+//  Created by 孙宇 on 15/12/2.
+//  Copyright © 2015年 孙宇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a[3]={1,2,3};
+    printf("a[0]=%d   a[1]=%d   a[2]=%d\n",a[0],a[1],a[2]);
+    int b[4];
+    printf("%x      %x\n",a,b);
+    int i;
+    for(i=1;i<10;i++)
+    {
+        b[i]=i;
+    }
+    printf("a[0]=%d   a[1]=%d   a[2]=%d\n",a[0],a[1],a[2]);
+    
+    return 0;
+}
